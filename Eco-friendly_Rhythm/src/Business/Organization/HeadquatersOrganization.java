e1c2e97f03ed64b7718ace01260de9ab9e523b84
apeksha-khandelwal <khandelwal.ap@northeastern.edu>
2020-12-05 22:35:44 -0500

    readding organization

---
+++ Eco-friendly_Rhythm/src/Business/Organization/HeadquatersOrganization.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.CareTaker;
+import Business.Role.Role;
+import Business.Role.Supplier;
+import java.util.ArrayList;
+import Business.Role.HeadquaterManager;
+import Business.Role.Investor;
+
+/**
+ * @author Alekhya
+ * @author Apeksha
+ * @author Shalini
+ */
+
+public class HeadquatersOrganization extends Organization {
+
+    public HeadquatersOrganization() {
+        super(Organization.Type.Headquaters.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new Supplier());
+        roles.add(new Investor());
+        roles.add(new HeadquaterManager());
+        roles.add(new CareTaker());
+        return roles;
+    }
+}
