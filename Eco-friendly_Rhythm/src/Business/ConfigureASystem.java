339a02065e83476a529b4671ae75f13c2c206adb
Alekhya Kadiri <kadiri.a@northeastern.edu>
2020-12-10 17:34:27 -0500

    added configureASystem in Business

--- Eco-friendly_Rhythm/src/Business/ConfigureASystem.java
+++ Eco-friendly_Rhythm/src/Business/ConfigureASystem.java
@@ -8,14 +8,17 @@ import Business.Organization.Organization;
 import Business.Role.AdminRole;
 import Business.Role.CareTaker;
 import Business.Role.Corporate;
+import Business.Role.Government;
 import Business.Role.HeadquaterManager;
 import Business.Role.Informer;
+import Business.Role.NGO;
 import Business.Role.Role;
 import Business.Role.Supplier;
 import Business.Role.SystemAdmin;
+import Business.User.User;
 import Business.UserAccount.UserAccount;
 import Business.UserAccount.UserAccountDirectory;
-import Business.WorkQueue.CordinatorWorkRequest;
+import Business.WorkQueue.InternalWorkRequest;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -24,47 +27,50 @@ import java.util.Date;
  * @author Apeksha
  * @author Shalini
  */
-
 public class ConfigureASystem {
-    
-    public static EcoSystem configure(){
-        
+
+    public static EcoSystem configure() {
+
         EcoSystem system = EcoSystem.getInstance();
-        
-        
+
         //create an enterprise
         //initialize some organizations
         //have some employees 
-        
         //create admin
         Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
-        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", "",employee, new SystemAdmin());
-        
+        UserAccount ua = system.getUserAccountDirectory().createEmployeeAccount("sysadmin", "sysadmin", "", employee, new SystemAdmin());
+
         //Create a network
-        Network network = new Network("BostonNetwork", "Boston","MA","US", new UserAccountDirectory(), new EnterpriseDirectory());
+        Network network = new Network("BostonNetwork", "Boston", "MA", "US");
         system.getNetworkList().add(network);
-        
+
         //create user
-        
-//        network.getUserAccountDirectory().createUserAccount("alekhya", "alekhya", "2323", u, new Informer());
+        User u = system.getUserDirectory().addUser();
+        u.setName("alekhya");
+        u.setUserName("alekhya");
+        u.setPassword("alekhya");
+        u.setNetwork(network);
+        u.setCompleteProfile(true);
+        UserAccount useracc = network.getUserAccountDirectory().createUserAccount("alekhya", "alekhya", "79890", u, new Informer());
+
         //create user account
         Employee employee2 = system.getEmployeeDirectory().createEmployee("admin");
-        UserAccount ua2 = network.getUserAccountDirectory().createUserAccount("admin", "apeksha", "",employee, new AdminRole());
-        
-        network.getEnterpriseDirectory().createAndAddEnterprise("BostonGovernment", 
+        UserAccount ua2 = network.getUserAccountDirectory().createEmployeeAccount("admin", "apeksha", "", employee, new AdminRole());
+
+        network.getEnterpriseDirectory().createAndAddEnterprise("BostonGovernment",
                 Enterprise.EnterpriseType.Government);
-        network.getEnterpriseDirectory().createAndAddEnterprise("BostonCorporate", 
+        network.getEnterpriseDirectory().createAndAddEnterprise("BostonCorporate",
                 Enterprise.EnterpriseType.Corporate);
-        network.getEnterpriseDirectory().createAndAddEnterprise("BostonHEadquarters", 
+        network.getEnterpriseDirectory().createAndAddEnterprise("BostonHEadquarters",
                 Enterprise.EnterpriseType.Headquaters);
-        network.getEnterpriseDirectory().createAndAddEnterprise("BostonNGO", 
+        network.getEnterpriseDirectory().createAndAddEnterprise("BostonNGO",
                 Enterprise.EnterpriseType.NGO);
-        
-        EnterpriseDirectory ed=network.getEnterpriseDirectory();
+
+        EnterpriseDirectory ed = network.getEnterpriseDirectory();
         Organization o;
-        
-        for(Enterprise ent: ed.getEnterpriseList()){
-            if(ent.getEnterpriseType()==Enterprise.EnterpriseType.Corporate){
+
+        for (Enterprise ent : ed.getEnterpriseList()) {
+            if (ent.getEnterpriseType() == Enterprise.EnterpriseType.Corporate) {
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.Corporate);
                 o.setName("OrgCorporate");
                 Employee e = new Employee();
@@ -72,11 +78,19 @@ public class ConfigureASystem {
                 e.setUserName("alekhyaCor");
                 e.setEmailId("alekhya");
                 e.setPassword("alekhya");
-                o.getUserAccountDirectory().createUserAccount("alekhya", "alekhya", "22", e, new Corporate());
-            }else if(ent.getEnterpriseType()==Enterprise.EnterpriseType.Government){
+                o.getUserAccountDirectory().createEmployeeAccount("alekhya", "alekhya", "22", e, new Corporate());
+            } else if (ent.getEnterpriseType() == Enterprise.EnterpriseType.Government) {
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.Government);
                 o.setName("OrgGovernment");
-            }else if(ent.getEnterpriseType()==Enterprise.EnterpriseType.Headquaters){
+                User gvrt = system.getUserDirectory().addUser();
+                gvrt.setName("gvrt");
+                gvrt.setUserName("gvrt");
+                gvrt.setPassword("government");
+                gvrt.setNetwork(network);
+                gvrt.setCompleteProfile(true);
+                o.getUserAccountDirectory().createUserAccount("gvrt", "government", "79890", gvrt, new Government());
+
+            } else if (ent.getEnterpriseType() == Enterprise.EnterpriseType.Headquaters) {
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.Headquaters);
                 o.setName("OrgHeadquarters");
                 Employee m = new Employee();
@@ -85,8 +99,8 @@ public class ConfigureASystem {
                 m.setUserName("manager");
                 m.setEmailId("manager");
                 m.setPassword("manager");
-                o.getUserAccountDirectory().createUserAccount("manager", "manager", "22", m, new HeadquaterManager());
-                
+                o.getUserAccountDirectory().createEmployeeAccount("manager", "manager", "22", m, new HeadquaterManager());
+
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.Investor);
                 o.setName("OrgInvestors");
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.CareTaker);
@@ -96,18 +110,19 @@ public class ConfigureASystem {
                 e.setUserName("shaliniCare");
                 e.setEmailId("shalini");
                 e.setPassword("shalini");
-                o.getUserAccountDirectory().createUserAccount("shaliniCare", "shalini", "22", e, new CareTaker());
-                CordinatorWorkRequest request = new CordinatorWorkRequest();
-            request.setTotalBill(20.00);
-           
-            request.setRequestDate(new Date());
-            request.setEmployee(e);
-            request.setCoordinatorAssigned(m);
-            request.setStatus("delivered");
-            request.setType("Headquarter Requirements");
-            o.getWorkQueue().getRequestList().add(request);
-                e.addUserRequest(request);
-                m.addUserRequest(request);
+                o.getUserAccountDirectory().createEmployeeAccount("shaliniCare", "shalini", "22", e, new CareTaker());
+                InternalWorkRequest request = new InternalWorkRequest();
+                request.setTotalBill(20.00);
+
+                request.setRequestDate(new Date());
+                request.setEmployee(e);
+                request.setCoordinatorAssigned(m);
+                request.setStatus("delivered");
+                request.setOverallStatus("Completed");
+                request.setType("Headquarter Requirements");
+                o.getWorkQueue().getRequestList().add(request);
+                e.addEmployeeRequest(request);
+                m.addEmployeeRequest(request);
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.Supplier);
                 o.setName("OrgSupplier");
                 Employee s = new Employee();
@@ -115,17 +130,22 @@ public class ConfigureASystem {
                 s.setUserName("supplier");
                 s.setEmailId("supplier");
                 s.setPassword("supplier");
-                o.getUserAccountDirectory().createUserAccount("supplier", "supplier", "22", s, new Supplier());
-            request.setSupplierAssigned(s);
-                s.addUserRequest(request);
-            }else{
+                o.getUserAccountDirectory().createEmployeeAccount("supplier", "supplier", "22", s, new Supplier());
+                request.setSupplierAssigned(s);
+                s.addEmployeeRequest(request);
+            } else {
                 o = ent.getOrganizationDirectory().createOrganization(Organization.Type.NGO);
                 o.setName("OrgNGO");
+                User s = new User();
+                s.setName("apeksha");
+                s.setUserName("apeksha");
+                s.setNetwork(network);
+                s.setEmailId("apeksha");
+                s.setPassword("apeksha");
+                o.getUserAccountDirectory().createUserAccount("apeksha", "apeksha", "22", s, new NGO());
             }
         }
-        
-        
-        
+
         return system;
-    }   
+    }
 }
